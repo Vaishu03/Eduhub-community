0b4229e3ebf2eeeb6f53241b5c442c6e2c16e5b8
Vaishu03 <vaishnaviswarnoj@example.com>
2023-01-08 15:57:35 +0530

    First html project

---
+++ Live coding qns/q1.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a[6] = {2,0,0,1,0,2},i=0,n=6,zero,one,two;
+    while(i<n){
+        if(a[i] == 0)
+        zero++;
+        else if
+    }
+}
